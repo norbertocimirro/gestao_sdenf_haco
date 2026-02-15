88bec417f123fc90f4eb926215ec20a09a5ea4a1
norbertocimirro <norbertocimirro@gmail.com>
2026-02-15 15:07:44 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -390,7 +390,7 @@ const MainSystem = ({ user, role, onLogout }) => {
       await fetch(API_URL_GESTAO, {
         method: 'POST',
         mode: 'no-cors',
-        headers: { 'Content-Type': 'text/plain' },
+        headers: { 'Content-Type': 'text/plain;charset=utf-8' },
         body: JSON.stringify({ action, payload })
       });
     } catch (e) {
